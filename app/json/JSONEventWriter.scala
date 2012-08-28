6e0f6dfad8b9031d4c7d695e44742e13038802bd
Alejandro Lujan <alex@tindr.ca>
2012-08-28 13:41:53 -0400

    JSON writer for Event objects

---
+++ app/json/JSONEventWriter.scala
@@ -0,0 +1,23 @@
+package json
+
+import models.S1Event
+
+import play.api.libs.json.Json.toJson
+import play.api.libs.json.JsValue
+import play.api.libs.json.Writes
+
+/** Writes an event to JSON
+  */
+object JsonEventWriter extends Writes[S1Event] {
+	override def writes(event: S1Event): JsValue = {
+		toJson(
+			Map("id" -> toJson(event.id.get),
+				"title" -> toJson(event.title),
+				"description" -> toJson(event.description),
+				"start" -> toJson(event.start.toString()),
+				"end" -> toJson(event.end.toString()),
+				"location" -> toJson(event.location),
+				"speakerId" -> toJson(event.speakerId))
+			)
+	}
+}
