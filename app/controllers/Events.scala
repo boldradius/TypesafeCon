514e730763a00dd62af49053facd9bfa92af5884
Alejandro Lujan <alex@tindr.ca>
2012-08-28 13:42:18 -0400

    Event list controller method

--- app/controllers/Events.scala
+++ app/controllers/Events.scala
@@ -2,37 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.S1Event
+import json.JsonEventWriter
 
-object Events extends Controller {
+object Events extends APIController {
 
-	// TODO implement
+	/** Returns the full list of events in JSON format
+	  */
 	def list = Action {
-		Ok("""{
-                 "status": "OK",
-                 "message": "Success",
-                 "result": {
-                   "events" : [
-                     {
-                       "id": 1, 
-                       "title": "Play! 101", 
-                       "description": "A great session for those with little or no experience in the Play Framework.", 
-                       "start": "2012-09-30 08:30:00", 
-                       "end": "2012-09-30 10:00:00",
-                       "location": "Room 42", 
-                       "speakerId": 2
-                     },
-                     {
-                       "id": 2, 
-                       "title": "Scala and functional programming", 
-                       "description": "An introduction to Scala in the context of functional programming.", 
-                       "start": "2012-10-01 10:15:00",
-                       "end": "2012-10-01 12:00:00",
-                       "location": "Room X", 
-                       "speakerId": 1
-                     }
-                   ]
-                 }
-              }""")
+		try {
+			Success(S1Event.findAll, "events")(JsonEventWriter)
+		} catch {
+			case t: Throwable => ServerError(t.getMessage)
+		}
 	}
 
 }
