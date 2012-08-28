cb3e7228394b07d027062d55ae07ac6706cd930b
Alejandro Lujan <alex@tindr.ca>
2012-08-28 12:27:13 -0400

    Adding create methods to models

--- app/models/S1Event.scala
+++ app/models/S1Event.scala
@@ -1,15 +1,71 @@
 package models
 
-import anorm._
+import java.util.Date
 import org.joda.time.DateTime
+import anorm.SqlParser._
+import anorm._
+import play.api.Play.current
+import play.api.db.DB
+import play.api.Logger
 
 case class S1Event(var id: Pk[Long],
-				title: String,
-				description: String,
-				start: DateTime,
-				end: DateTime,
-				location: String,
-				speakerId: Long) {
+	title: String,
+	description: String,
+	start: DateTime,
+	end: DateTime,
+	location: String,
+	speakerId: Long) {
+
+	/** Inserts the event into the DB
+	  */
+	def create = {
+		Logger.debug("Creating Event " + this)
+
+		DB.withConnection { implicit connection =>
+			val id = SQL("""insert into event (title, description, starttime, endtime, location, speakerid) 
+							values ({title}, {description}, {start}, {end}, {location}, {speakerid})""").on(
+				'title -> title,
+				'description -> description,
+				'start -> new Date(start.getMillis()),
+				'end -> new Date(end.getMillis()),
+				'location -> location,
+				'speakerid -> speakerId).executeInsert()
+
+			id.map {
+				case id => {
+					this.id = Id(id)
+					this
+				}
+			}
+		}
+	}
+
 }
 
-object S1Event {}
+object S1Event {
+
+	/**
+	 * Parses a row into an event
+	 */
+	private val event = {
+		get[Pk[Long]]("id") ~
+			get[String]("title") ~
+			get[String]("description") ~
+			get[Date]("start") ~
+			get[Date]("end") ~
+			get[String]("location") ~
+			get[Long]("speakerid") map {
+				case id ~ title ~ description ~ start ~ end ~ location ~ speakerId =>
+					S1Event(id, title, description, new DateTime(start), new DateTime(end), location, speakerId)
+			}
+	}
+
+	/**
+	 * Fetches all Events
+	 */
+	def findAll = {
+		DB.withConnection { implicit connection =>
+			SQL("select * from event").as(event *)
+		}
+	}
+}
--- app/models/Speaker.scala
+++ app/models/Speaker.scala
@@ -2,15 +2,65 @@ package models
 
 import anorm._
 import org.joda.time.DateTime
+import anorm.SqlParser._
+import anorm._
+import play.api.Play.current
+import play.api.db.DB
+import play.api.Logger
 
 case class Speaker(var id: Pk[Long],
 				name: String,
 				title: String,
 				about: String,
-				email: String,
-				twitter: String,
-				url: String) {
-
+				email: Option[String],
+				twitter: Option[String],
+				url: Option[String]) {
+	
+	/** Inserts the speaker into the DB
+	  */
+	def create = {
+		Logger.debug("Creating Speaker " + this)
+		
+		DB.withConnection { implicit connection =>
+			val id = SQL("""insert into speaker (name, title, about, email, twitter, url) 
+							values ({name}, {title}, {about}, {email}, {twitter}, {url})""").on(
+						'name -> name,
+						'title -> title,
+						'about -> about,
+						'email -> email,
+						'twitter -> twitter,
+						'url -> url).executeInsert()
+						
+			id.map {
+				case id => {
+					this.id = Id(id)
+					this
+				}
+			}
+		}
+	}
 }
 
-object Speaker {}
+object Speaker {
+	
+	private val speaker = {
+		get[Pk[Long]]("id") ~
+		get[String]("name") ~
+		get[String]("title") ~
+		get[String]("about") ~
+		get[Option[String]]("email") ~ 
+		get[Option[String]]("twitter") ~ 
+		get[Option[String]]("url") map {
+			case id ~ name ~ title ~ about ~ email ~ twitter ~ url => 
+				Speaker(id, name, title, about, email, twitter, url)
+		}
+	}
+		
+	/** Fetches all Events
+	  */
+	def findAll = {
+		DB.withConnection { implicit connection =>
+			SQL("select * from speaker").as(speaker *)
+		}
+	}
+}
