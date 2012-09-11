c7f2aecdb53ea4380cdfeec052c7df68ce758eb8
Alejandro Lujan <alex@tindr.ca>
2012-09-11 13:14:46 -0400

    Minor cleanup

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,24 +13,4 @@ object Application extends Controller {
 	def playground = Action {
 		implicit request => Ok(views.html.playground())
 	}
-
-	// TODO remove
-	def test = Action {
-		implicit request =>
-
-			// Default channel and events created by Pusher for a new app
-			val channel = "another"
-			val event = "hi"
-			val message = "Hello from the Typesafe app!"
-
-			// Send message through Pusher and catch response
-			val promise = Pusher().trigger(channel, event, message)
-			val resp = promise.await(10000)
-
-			// Check status and body
-			Logger.debug("Status: " + resp.get.status)
-			Logger.debug("Body: " + resp.get.body.toString())
-
-			Ok(views.html.index())
-	}
 }
