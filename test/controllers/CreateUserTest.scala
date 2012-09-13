1a826c3bfeac2d873713cf267ecab1b522d7c826
Alejandro Lujan <alex@tindr.ca>
2012-09-13 13:50:20 -0400

    Refactoring CreateUserTest

--- test/controllers/CreateUserTest.scala
+++ test/controllers/CreateUserTest.scala
@@ -8,117 +8,90 @@ import play.api.test.Helpers._
 import play.api.test._
 
 class CreateUserTest extends APISpecification {
-	
+
 	"The Create User API call" should {
-		
+
 		"Return error when token is not provided" in new CreateUserTestCase {
 			running(FakeApplication()) {
 				val Some(result) = routeAndCall(FakeRequest(POST, "/users").withFormUrlEncodedBody("firstName" -> "John"))
-					
-				status(result) must beEqualTo(BAD_REQUEST)
-				contentType(result) must beSome("application/json")
-				
-				contentAsString(result) match {
-					case ValidResponse(status, message, result) => message must beEqualTo("Unauthorized access")
-					case content => failure("Invalid response format: '" + content + "'")
-				}
+				verifyBadResult(result, "Unauthorized access")
 			}
 		}
-		
+
 		"Return error when token is incorrect" in new CreateUserTestCase {
 			running(FakeApplication()) {
 				val Some(result) = routeAndCall(FakeRequest(POST, "/users").withFormUrlEncodedBody("firstName" -> "John", "token" -> invalidToken))
-					
-				status(result) must beEqualTo(BAD_REQUEST)
-				contentType(result) must beSome("application/json")
-				
-				contentAsString(result) match {
-					case ValidResponse(status, message, result) => message must beEqualTo("Unauthorized access")
-					case content => failure("Invalid response format: '" + content + "'")
-				}
+				verifyBadResult(result, "Unauthorized access")
 			}
 		}
-		
+
 		"Return error when email is not provided" in new CreateUserTestCase {
 			running(FakeApplication()) {
 				val Some(result) = routeAndCall(FakeRequest(POST, "/users").withFormUrlEncodedBody("firstName" -> "John", "token" -> validToken))
-					
-				status(result) must beEqualTo(BAD_REQUEST)
-				contentType(result) must beSome("application/json")
-				
-				contentAsString(result) match {
-					case ValidResponse(status, message, result) => message must beEqualTo("Missing parameter: email")
-					case content => failure("Invalid response format: '" + content + "'")
-				}
+				verifyBadResult(result, "Missing parameter: email")
 			}
 		}
-		
-		
+
 		"Return error when creating user with an email already registered" in new CreateUserTestCase {
 			running(FakeApplication()) {
 				val Some(result) = routeAndCall(FakeRequest(POST, "/users").withFormUrlEncodedBody(
-					"firstName" -> "John", 
-					"email" -> "john@example.com", 
+					"firstName" -> "John",
+					"email" -> "john@example.com",
 					"token" -> validToken))
-					
+
 				val Some(result2) = routeAndCall(FakeRequest(POST, "/users").withFormUrlEncodedBody(
-					"firstName" -> "Peter", 
-					"email" -> "john@example.com", 
+					"firstName" -> "Peter",
+					"email" -> "john@example.com",
 					"token" -> validToken))
-					
-				status(result2) must beEqualTo(BAD_REQUEST)
-				contentType(result2) must beSome("application/json")
-				
-				contentAsString(result2) match {
-					case ValidResponse(status, message, result) => message must beEqualTo("Email is already registered")
-					case content => failure("Invalid response format: '" + content + "'")
-				}
+
+				verifyBadResult(result, "Email is already registered")
 			}
-		}	
-		
-		
+		}
+
 		"Create a user when passed all valid parameters" in new CreateUserTestCase {
 			running(FakeApplication()) {
 				val Some(result) = routeAndCall(FakeRequest(POST, "/users").withFormUrlEncodedBody(
 					"firstName" -> "John",
 					"lastName" -> "Doe",
-					"twitter" -> "john", 
+					"twitter" -> "john",
 					"facebook" -> "johnDoe",
-					"phone" -> "987-6543210", 
-					"email" -> "john@example.com", 
-					"website" -> "http://example.com", 
+					"phone" -> "987-6543210",
+					"email" -> "john@example.com",
+					"website" -> "http://example.com",
 					"token" -> validToken))
-				status(result) must beEqualTo(OK)
-				contentType(result) must beSome("application/json")
-				
-				val IdResponse = """.+"id":(\d+).+""".r
-				
-				contentAsString(result) match {
-					case IdResponse(id) => 
-						User.findById(id.toLong) match {
-							case None => failure("User with id " + id + " was not found")
-							case Some(user) => {
-								user.firstName must beEqualTo(Some("John"))
-								user.lastName must beEqualTo(Some("Doe"))
-								user.twitter must beEqualTo(Some("john"))
-								user.facebook must beEqualTo(Some("johnDoe"))
-								user.phone must beEqualTo(Some("987-6543210"))
-								user.email must beEqualTo("john@example.com")
-								user.website must beEqualTo(Some("http://example.com"))
+
+				verifyGoodResult(result) {
+					response =>
+						{
+							val IdJson = """"id":(\d+)""".r
+							response.message match {
+								case IdJson(id) => {
+									User.findById(id.toLong) match {
+										case None => failure("User with id " + id + " was not found")
+										case Some(user) => {
+											user.firstName must beEqualTo(Some("John"))
+											user.lastName must beEqualTo(Some("Doe"))
+											user.twitter must beEqualTo(Some("john"))
+											user.facebook must beEqualTo(Some("johnDoe"))
+											user.phone must beEqualTo(Some("987-6543210"))
+											user.email must beEqualTo("john@example.com")
+											user.website must beEqualTo(Some("http://example.com"))
+										}
+									}
+								}
+								case json => failure("Response does not contain valid id: " + contentAsString(result))
 							}
 						}
-					case json => failure("Response does not contain valid id: " + json)
 				}
-				
 			}
 		}
 	}
 }
 
 trait CreateUserTestCase extends After {
-	
+
 	// Remove the test data
-	def after = running(FakeApplication()) { 
+	def after = running(FakeApplication()) {
 		User.findByEmail("john@example.com").map(_.delete)
 	}
 }
