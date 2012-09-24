f68b581489ace19f236f6aaa781a210fa6587c16
Alejandro Lujan <alex@tindr.co>
2012-09-24 11:05:05 -0400

    Cleaning up url on speaker page

--- app/tools/StringTools.scala
+++ app/tools/StringTools.scala
@@ -2,4 +2,7 @@ package tools
 
 object StringTools {
 	implicit def string2OptionString(s:String) = Some(s)
+	
+	def cleanurl(url: String) = url.replaceAll("http.?://","").replaceAll("/$", "") 
+
 }
